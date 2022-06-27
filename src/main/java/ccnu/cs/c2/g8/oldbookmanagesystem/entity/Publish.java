7f82b3f4edd70e3f1b2578faf1a63dd254e08c69
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-27 09:47:30 +0800

    0.9.2 by xxy

---
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/entity/Publish.java
@@ -0,0 +1,34 @@
+package ccnu.cs.c2.g8.oldbookmanagesystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PUBLISH")
+public class Publish {
+    @Id
+    @Column(name = "Uno")
+    private String uno;
+
+    @Id
+    @Column(name = "Bno")
+    private String bno;
+
+    public String getUno() {
+        return uno;
+    }
+
+    public void setUno(String uno) {
+        this.uno = uno;
+    }
+
+    public String getBno() {
+        return bno;
+    }
+
+    public void setBno(String bno) {
+        this.bno = bno;
+    }
+}
---
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/entity/Want.java
@@ -0,0 +1,34 @@
+package ccnu.cs.c2.g8.oldbookmanagesystem.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "WANT")
+public class Want {
+    @Id
+    @Column(name = "Bno")
+    private String bno;
+
+    @Id
+    @Column(name = "Uno")
+    private String uno;
+
+    public String getBno() {
+        return bno;
+    }
+
+    public void setBno(String bno) {
+        this.bno = bno;
+    }
+
+    public String getUno() {
+        return uno;
+    }
+
+    public void setUno(String uno) {
+        this.uno = uno;
+    }
+}
