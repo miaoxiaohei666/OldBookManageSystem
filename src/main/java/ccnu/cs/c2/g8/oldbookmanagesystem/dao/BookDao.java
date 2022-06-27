3a390201068ead1e976a878ddcd0ee5f7ce3017e
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-27 20:25:55 +0800

    0.9.10 by xxy

--- src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/dao/BookDao.java
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/dao/BookDao.java
@@ -2,6 +2,20 @@ package ccnu.cs.c2.g8.oldbookmanagesystem.dao;
 
 import ccnu.cs.c2.g8.oldbookmanagesystem.data.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface BookDao extends JpaRepository<Book,String> {
+import java.util.List;
+
+public interface BookDao extends JpaRepository<Book, String> {
+    List<Book> findByUno(Integer uno);
+
+    @Query(value = "update Book set Book.bprice=price where Book.bno=Bno")
+    Integer updateBookBpriceByUno(Integer Bno, Integer price);
+
+    @Query(value = "update Book set Book.bdescibe=describe where Book.bno=Bno")
+    Integer updateBookBdescribeByUno(Integer Bno, String describe);
+
+    @Query(value = "update Book set Book.bstate=state where Book.bno=Bno")
+    Integer updateBookBstateByUno(Integer Bno, boolean state);
+
 }
--- src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/dao/UserDao.java
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/dao/UserDao.java
@@ -1,9 +1,26 @@
 package ccnu.cs.c2.g8.oldbookmanagesystem.dao;
 
 
+import ccnu.cs.c2.g8.oldbookmanagesystem.data.entity.Book;
 import ccnu.cs.c2.g8.oldbookmanagesystem.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserDao extends JpaRepository<User,Integer> {
     public  User findUserByUno(Integer uno);
+
+    @Query(value="update User set User.unickname=nickname where User.uno=Uno")
+    Integer updateUserunicknameByUno(Integer Uno,String nickname);
+
+    @Query(value="update User set User.upassword=password where User.uno=Uno")
+    Integer updateUserUpasswordByUno(Integer Uno,String password);
+
+    @Query(value="select Bname,Create_time,Bprice,Bdescribe,Bstate from Book ,Publish where Publish.uno=Uno and Publish.bno=Book .bno",nativeQuery = true)
+    List<Book> findPublishByUno(Integer Uno);
+
+    @Query(value="select Bname,Create_time,Bprice,Bdescribe,Bstate from Book ,Want where Want.uno=Uno and Want.bno=Book.bno")
+    List<Book> findWantByUno(Integer Uno);
+
 }
