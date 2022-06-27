c3f9410084f11da5bd16f4b07d9fa1566bf6137f
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-27 19:32:42 +0800

    0.9.7 by xxy

--- Database_CreatTable.sql
+++ Database_CreatTable.sql
@@ -5,7 +5,7 @@ drop table if exists USER;
 create table USER
 (Uno numeric(10,0) not null primary key,
  Ugrade numeric(4,0) not null,
- Unme varchar(255) not null,
+ Uname varchar(255) not null,
  Unickname varchar(255) not null,
  Utel numeric(11,0) not null,
  Uwechat varchar(255) default null,
@@ -16,7 +16,17 @@ create table USER
  Unlike int default null
 );
 
-INSERT INTO `USER` VALUES (2020213673,2020,'xxy','miaoxiaohei',15892603115,'15892603115','1521260640',0,0,0);
+
+INSERT INTO USER VALUES (2020213673,2020,'xxy','miaoxiaohei',15892603115,'15892603115','1521260640',12345678,0,0,0);
+
+-- ----------------------------
+-- SORT
+-- ----------------------------
+drop table if exists SORT;
+create table SORT
+(Sno numeric(10,0) not null primary key,
+ Sname varchar(255) not null
+);
 
 -- ----------------------------
 -- BOOK
@@ -31,7 +41,7 @@ create table BOOK
  Bprice int not null,
  Bdescribe text not null,
  Bstate bit default null,
- foreign key (Sno) references SORT(Sno),
+ foreign key (Sno) references SORT(Sno)
 );
 
 -- ----------------------------
@@ -54,13 +64,4 @@ create table WANT
  Uno numeric(10,0) not null,
  foreign key (Bno) references BOOK(Bno),
  foreign key (Uno) references USER(Uno)
-);
-
--- ----------------------------
--- WANT
--- ----------------------------
-drop table if exists SORT;
-create table SORT
-(Sno numeric(10,0) not null primary key,
- Sname varchar(255) not null
 );
--- sort.sql
+++
@@ -1,6 +0,0 @@
-drop table if exists SORT;
-
-create table SORT
-	(Sno numeric(10,0) not null primary key,
-	 Sname varchar(255) not null
-	 );
