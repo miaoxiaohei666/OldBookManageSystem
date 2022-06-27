f71c8b0619a6fdb55c1498ddb707f901d38f1a6a
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-27 09:55:38 +0800

    0.9.2 by xxy

--- src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/biz/UserBiz.java
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/biz/UserBiz.java
@@ -1,4 +1,11 @@
 package ccnu.cs.c2.g8.oldbookmanagesystem.biz;
 
+import ccnu.cs.c2.g8.oldbookmanagesystem.dao.UserDao;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserBiz {
+    private UserDao userDao;
 }
--- src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/control/UserControl.java
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/control/UserControl.java
@@ -1,4 +1,12 @@
 package ccnu.cs.c2.g8.oldbookmanagesystem.control;
 
+import ccnu.cs.c2.g8.oldbookmanagesystem.biz.UserBiz;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 public class UserControl {
+    @Autowired
+    UserBiz userBiz;
 }
