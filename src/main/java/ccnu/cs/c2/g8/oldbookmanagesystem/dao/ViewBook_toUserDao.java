5098da641463d6f11669b457d1cc57497d630fef
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-27 15:30:39 +0800

    0.9.5 by xxy

---
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/dao/ViewBook_toUserDao.java
@@ -0,0 +1,7 @@
+package ccnu.cs.c2.g8.oldbookmanagesystem.dao;
+
+import ccnu.cs.c2.g8.oldbookmanagesystem.data.vo.ViewBook_toUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ViewBook_toUserDao extends JpaRepository <ViewBook_toUser,String> {
+}
