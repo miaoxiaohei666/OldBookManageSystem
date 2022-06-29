904c58a325aa3fc8f99d36926afc493ec1e33b08
miaoxiaohei <miaoxiaohei666@gmail.com>
2022-06-29 19:50:19 +0800

    0.9.34 by xxy

--- src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/OldBookManageSystemApplication.java
+++ src/main/java/ccnu/cs/c2/g8/oldbookmanagesystem/OldBookManageSystemApplication.java
@@ -1,9 +1,9 @@
 package ccnu.cs.c2.g8.oldbookmanagesystem;
 
-import ccnu.cs.c2.g8.oldbookmanagesystem.data.entity.Book;
-import ccnu.cs.c2.g8.oldbookmanagesystem.data.entity.User;
+
 import ccnu.cs.c2.g8.oldbookmanagesystem.service.UserService;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -11,11 +11,6 @@ public class OldBookManageSystemApplication {
 
     public static void main(String[] args) {
         //SpringApplication.run(OldBookManageSystemApplication.class, args);
-        UserService userService = new UserService();
-        if (userService.isUser(2020213673)){
-            System.out.println("ok!");
-        }
-        else System.out.println("no");
-    }
 
+    }
 }
